94ed04385a4ac75f4c0a11490be1697b7d6d07c2
KJ Coop <git@kjcoop.com>
2019-06-06 10:18:56 -0700

    The basic functionality is in place.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title    = "KJ's Sample Gallery";
-  subtitle = 'Not that the world has a shortage of gallery applications';
+    placeholder = "Click an image to see a larger version";
+
+    showImage(event) {
+        var parts = event.target.src.split("/");
+        var img = parts[parts.length - 1];
+        var full = document.getElementById('full');
+        full.innerHTML = '<img src="assets/lg/' + img + '">';
+    }
 }
---
+++ src/assets/README.md
@@ -0,0 +1,5 @@
+## KJ's Sample Angular Gallery
+
+I wanted a project to tinker around with Angular. I have tinkered.
+
+
